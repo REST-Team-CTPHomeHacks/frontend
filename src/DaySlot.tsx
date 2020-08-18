03e9007906e9ee9d7491ed1235fc147bfe4892b3
rG480 <55470594+rG480@users.noreply.github.com>
2020-08-18 16:56:29 -0400

    Latest stuff

--- src/DaySlot.tsx
+++ src/DaySlot.tsx
@@ -1,9 +1,24 @@
 import * as React from 'react';
 import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Collapse from 'react-bootstrap/Collapse';
 
 export class DaySlot extends React.Component {
+   
+      
     render(){
         return <div>
+   
+
+
+
+            <Card style= {{backgroundColor: "green", width: '18rem' }}
+            className="mb-2">
+             <Card.Header>Monday</Card.Header>
+             <Card.Body>This is some text within a card body.
+            
+             </Card.Body>
+           </Card>
             
           
             <Button>Hello World!</Button>
