ed6ed5b52921efef5fead9504d8cf120a32e46a1
rG480 <55470594+rG480@users.noreply.github.com>
2020-08-18 17:02:56 -0400

    Added comments to the day Component

--- src/DaySlot.tsx
+++ src/DaySlot.tsx
@@ -10,13 +10,18 @@ export class DaySlot extends React.Component {
         return <div>
    
 
-
+           {/*The gist of things is that we want to use a card to display each day.
+            The card can be split into a header and body section*/ }
 
             <Card style= {{backgroundColor: "green", width: '18rem' }}
             className="mb-2">
+                  {/*Header should always contain the day*/}
+
              <Card.Header>Monday</Card.Header>
              <Card.Body>This is some text within a card body.
-            
+             {/* Body should contain a little message depending on the work/leisure ratio. 
+             Should also show the exact hours spent on work and leisure
+             We also want to add the collapse in this card body if possible. */}
              </Card.Body>
            </Card>
             
